71500f38d24546ca6d2cd1d856f63d5d7935a7c3
popopo19 <46850477+popopo19@users.noreply.github.com>
2019-02-28 22:41:48 -0600

    KINDA STARTED
    KINDA STARTED ON MOVEMENTS.

---
+++ Space Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D playerRb;
+
+    [SerializeField] float speed;
+    [SerializeField] Vector2 playerDir;
+    [SerializeField] float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        RocketOn();
+        //Steering();
+    }
+
+    private void RocketOn()
+    {
+
+    }
+
+    private void Steering()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerRb.AddForce(playerDir * speed);
+        }
+    }
+}
