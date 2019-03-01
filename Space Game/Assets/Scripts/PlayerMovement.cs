986bc8c1c942c217405f6a7e8f760b7f52bf8146
popopo19 <46850477+popopo19@users.noreply.github.com>
2019-03-01 17:09:51 -0600

    MOVEMENT DONE
    IM DONE WITH MOVEMENT

--- Space Game/Assets/Scripts/PlayerMovement.cs
+++ Space Game/Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody2D playerRb;
 
     [SerializeField] float speed;
-    [SerializeField] Vector2 playerDir;
     [SerializeField] float horizontal;
+    [SerializeField] float vertical;
+    [SerializeField] float angle;
+    [SerializeField] Vector2 dir;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 10f;
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
+
+        speed = 100f;
 
     }
 
@@ -21,24 +25,55 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        angle = transform.eulerAngles.z;
+
+        DirectionFunction();
+        Steering(horizontal);
     }
 
     private void FixedUpdate()
     {
-        RocketOn();
-        //Steering();
-    }
-
-    private void RocketOn()
-    {
+        RocketOn(dir);
 
     }
 
-    private void Steering()
+    private void RocketOn(Vector2 dir)
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            playerRb.AddForce(playerDir * speed);
+
+            playerRb.AddForce(dir * speed * Time.fixedDeltaTime);
         }
+    }
+
+    private void Steering(float horizontal)
+    {
+        transform.Rotate(0f, 0f, -horizontal * 5f);
+    }
+
+    void DirectionFunction()
+    {
+        float curSin = 0;
+        float curCos = 0;
+
+
+
+        if (angle > 90 && 270 > angle)
+        {
+            curSin = Mathf.Sin(transform.rotation.z);
+            curCos = Mathf.Cos(transform.rotation.z) * -1;
+
+            Debug.Log("Facing Left");
+
+        } else
+        {
+            curSin = Mathf.Sin(transform.rotation.z);
+            curCos = Mathf.Cos(transform.rotation.z);
+        }
+
+        Debug.Log(curCos + " " + curSin);
+
+        dir = new Vector2(curCos, curSin);
     }
 }
